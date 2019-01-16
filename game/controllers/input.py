2eba9ae5af2376f89b1560131b341d4edf69b8e3
dbulka <dzm.bulka@gmail.com>
2019-01-16 21:56:25 +0300

    perfom saving from keyboard

--- game/controllers/input.py
+++ game/controllers/input.py
@@ -1,10 +1,14 @@
 import keyboard
-from pyQAtest_tamagochi.game.views import GameInterface, User
-from pyQAtest_tamagochi.game.models import pets
-from pyQAtest_tamagochi.utils import JSONstorage
 
+from pyQAtest_tamagochi.utils import JSONstorage
+from pyQAtest_tamagochi.game.views import gaming
+
+
+
+#initializate json_storage
+storage = JSONstorage()
 
 while True:
-    if keyboard.is_pressed('s'):
-        storage.write_data(pet.name, n, pet.alive)
+    if keyboard.is_pressed('^') and keyboard.is_pressed('S'):
+        storage.write_data(gaming.pet.name, gaming.n, gaming.pet.alive)
 
